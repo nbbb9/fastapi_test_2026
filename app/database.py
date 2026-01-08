b387f052b3928dbf574f23da8f5684ca4b980305
이영원 <nbbb9@naver.com>
2026-01-08 13:42:59 +0900

    feat: 민감정보를 .env에서 조회하도록 수정한다.

--- app/database.py
+++ app/database.py
@@ -5,18 +5,23 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# 1. 환경 변수에서 스키마 이름 가져오기 (없으면 test 기본값)
-TARGET_SCHEMA = os.getenv("DB_SCHEMA", "test")
+# 1. 환경 변수
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", "5432")
+DB_USER = os.getenv("DB_USER", "root")
+DB_PW = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
+DB_SCHEMA = os.getenv("DB_SCHEMA", "test")
 
 # 2. 데이터베이스 연결 정보
 # SQLALCHEMY_DATABASE_URL = "postgresql://<사용자>:<비밀번호>@<호스트>:<포트>/<DB명>"
-SQLALCHEMY_DATABASE_URL = "postgresql://myuser:mypassword@127.0.0.1:5432/mydb"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PW}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 # 3. 엔진 생성
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
     connect_args={
-        "options": f"-c search_path={TARGET_SCHEMA}"
+        "options": f"-c search_path={DB_SCHEMA}"
     }
 )
 
@@ -27,15 +32,12 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 # 5. Base 클래스 (ORM 모델 상속용, 필요시 사용)
 Base = declarative_base()
 
-# ---------------------------------------------------------
 # 6. 프로시저 자동 로드 함수
-# ---------------------------------------------------------
 PROCEDURE_DIR = "app/db/procedures"  # SQL 파일들이 위치한 경로
 
 def load_stored_procedures(db: Session):
     """
-    서버 시작 시 지정된 폴더의 모든 프로시저(SQL)를 DB에 적용합니다.
-    (파일별 독립 트랜잭션 적용)
+    서버 시작 시 지정된 폴더의 모든 프로시저(SQL)를 DB에 적용합니다. (파일별 독립 트랜잭션 적용)
     """
     if not os.path.exists(PROCEDURE_DIR):
         print(f"[알림] 프로시저 경로를 찾을 수 없음: {PROCEDURE_DIR}")
@@ -56,13 +58,11 @@ def load_stored_procedures(db: Session):
 
                 if sql_content.strip():
                     db.execute(text(sql_content))
-                    # 성공하면 즉시 반영 (다른 파일에 영향 안 줌)
-                    db.commit()
+                    db.commit() # 성공하면 즉시 반영 (다른 파일에 영향 안 줌)
                     print(f"[성공] {filename} 적용 완료")
 
         except Exception as e:
-            # 실패 시 롤백하여 트랜잭션을 깨끗하게 비움
-            db.rollback()
+            db.rollback() # 실패 시 롤백
             print(f"[실패] {filename} 적용 중 에러 발생: {e}")
             # 여기서 롤백을 했으므로, 다음 파일은 정상적인 새 트랜잭션에서 실행됨
 
